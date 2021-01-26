930ebd2199f4dd612e726ddf2a66ac5b967c4800
Dan Moore <github@mooreds.com>
2021-01-26 16:56:48 -0700

    Partial solution for dynamic client ids.

--- hooli/routes/index.js
+++ hooli/routes/index.js
@@ -3,27 +3,24 @@ const router = express.Router();
 const {FusionAuthClient} = require('@fusionauth/typescript-client');
 
 
-const clientId = '4d04696e-95dc-4deb-8f53-daea1d11ae47';
-const clientSecret = '-5jYS2iKd4iLX4P1GPe-Z3XGnHJg0rz25sKyPUsuteQ';
-const client = new FusionAuthClient('noapikeyneeded', 'http://localhost:9011');
+const client = new FusionAuthClient('REAL API KEY NEEDED', 'http://localhost:9011');
 const hostName = 'hooli.local';
 const port = 3001;
 
-const loginUrl = 'http://localhost:9011/oauth2/authorize?client_id='+clientId+'&response_type=code&redirect_uri=http%3A%2F%2F'+hostName+'%3A'+port+'%2Foauth-redirect&scope=offline_access';
 const logoutUrl = 'http://localhost:9011/oauth2/logout?client_id='+clientId;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-
+  
   if (!req.session.user) {
-     res.redirect(302, loginUrl);
+     res.redirect(302, loginUrl());
   }
-  res.render('index', {user: req.session.user, title: 'Hooli App', clientId: clientId, logoutUrl: "/logout", loginUrl: loginUrl});
+  res.render('index', {user: req.session.user, title: 'Hooli App', clientId: clientId, logoutUrl: "/logout", loginUrl: loginUrl()});
 });
 
 /* Login page if we aren't logged in */
 router.get('/login', function (req, res, next) {
-  res.render('login', {title: 'Hooli Login', clientId: clientId, loginUrl: loginUrl});
+  res.render('login', {title: 'Hooli Login', clientId: clientId, loginUrl: loginUrl()});
 });
 
 /* Logout page */
@@ -41,7 +38,20 @@ router.get('/endsession', function (req, res, next) {
 /* OAuth return from FusionAuth */
 router.get('/oauth-redirect', function (req, res, next) {
   // This code stores the user in a server-side session
-  client.exchangeOAuthCodeForAccessToken(req.query.code,
+
+   const tenant = req.query.state;
+   const clientId = "";
+   const clientSecret = "";
+   client.retrieveSystemConfiguration()
+      .then((response) => {
+        const domainToApplicationIdMapping = response.response.systemConfiguration.data.domainToApplicationIdMapping; // previously managed by API calls to https://fusionauth.io/docs/v1/tech/apis/system/#update-the-system-configuration
+        const myClientInfo = domainToApplicationIdMapping[referrer]; // or just have the applicationId and do another API call
+        if (myClientInfo.applicationId) {
+          clientId = myClientInfo.clientId;
+          clientSecret = myClientInfo.secret;
+     
+          // callback heck
+          client.exchangeOAuthCodeForAccessToken(req.query.code,
                                          clientId,
                                          clientSecret,
                                          'http://'+hostName+':'+port+'/oauth-redirect')
@@ -60,6 +70,23 @@ router.get('/oauth-redirect', function (req, res, next) {
       .then((response) => {
         res.redirect(302, '/');
       }).catch((err) => {console.log("in error"); console.error(JSON.stringify(err));});
+      }
+   });
 });
+
+loginUrl() => () {
+   const referrer = '...'; // dynamically get referrer
+   client.retrieveSystemConfiguration()
+      .then((response) => {
+        const domainToApplicationIdMapping = response.response.systemConfiguration.data.domainToApplicationIdMapping; // previously managed by API calls to https://fusionauth.io/docs/v1/tech/apis/system/#update-the-system-configuration
+        const myClientInfo = domainToApplicationIdMapping[referrer];
+        if (myClientInfo.applicationId) {
+          clientId = myClientInfo.clientId;
+          return = 'http://localhost:9011/oauth2/authorize?client_id='+clientId+'&response_type=code&redirect_uri=http%3A%2F%2F'+hostName+'%3A'+port+'%2Foauth-redirect&scope=offline_access&state='+referrer;
+        }
+       }
+   return ""; //error state
+}
+
 
 module.exports = router;
